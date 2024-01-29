03e12723d477d358b8620cf36bcf005a2ca3b26b
Prathyarthi <prathyarti@gmail.com>
2024-01-29 19:11:04 +0530

    Added arrays

--- typescript/node-app/src/a.ts
+++ typescript/node-app/src/a.ts
@@ -100,3 +100,23 @@ function greet(firstName: greetArgs) {
 
 greet("Prathu")
 greet(1)
+
+
+// Intersection
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+interface Manager {
+    name: string;
+    department: string;
+};
+
+type TeamLead = Employee & Manager;
+
+const teamLead: TeamLead = {
+    name: "harkirat",
+    startDate: new Date(),
+    department: "Software developer"
+};
