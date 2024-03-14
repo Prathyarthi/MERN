0cb83dd713603ae34172d9ce920d1911a859a978
Prathyarthi <prathyarti@gmail.com>
2024-03-14 16:45:13 +0530

    Added relationships

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -10,3 +10,43 @@ async function insertUser(email: string, password: string, firstname: string) {
         }
     })
 }
+
+
+async function createTodo(userId: number, title: string, description: string) {
+    await prisma.todo.create({
+        data: {
+            userId,
+            title,
+            description,
+        }
+    })
+}
+
+createTodo(1, "go to gym", "go to gym and do 10 pushups");
+
+async function getTodos(userId: number,) {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: userId,
+        },
+    });
+    console.log(todos);
+}
+
+getTodos(1);
+
+async function getTodosAndUserDetails(userId: number,) {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: userId,
+        },
+        select: {
+            user: true,
+            title: true,
+            description: true
+        }
+    });
+    console.log(todos);
+}
+
+getTodosAndUserDetails(1);
