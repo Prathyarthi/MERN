6f1b08a01e91ec6eb083acb2c1856823c5d9036e
Prathyarthi <prathyarti@gmail.com>
2024-04-13 21:23:54 +0530

    Implemented polling like architecture using queue

--- redis/primary_backend/src/index.ts
+++ redis/primary_backend/src/index.ts
@@ -8,14 +8,14 @@ const client = createClient();
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 app.post("/submit", async (req, res) => {
+    const userId = req.body.userId;
     const problemId = req.body.problemId;
     const code = req.body.code;
     const language = req.body.language;
 
     try {
-        await client.lPush("problems", JSON.stringify({ code, language, problemId }));
-        // Store in the database
-        res.status(200).send("Submission received and stored.");
+        await client.lPush("problems", JSON.stringify({ userId, code, language, problemId }));
+        res.status(200).send("Submission received.");
     } catch (error) {
         console.error("Redis error:", error);
         res.status(500).send("Failed to store submission.");
