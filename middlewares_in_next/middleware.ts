ea6b9ca0caa3a37cd724717ac8521e0f671336ce
Prathyarthi <prathyarti@gmail.com>
2024-04-23 20:56:21 +0530

    Added middleware logic to selectively run for a particular route

--- middlewares_in_next/middleware.ts
+++ middlewares_in_next/middleware.ts
@@ -1,20 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 
 // For Analytics usecase such as request count
-let requestCount = 0;
+// let requestCount = 0;
 
-// This middleware runs on all routes
-export function middleware(req: NextRequest) {
-    requestCount++;
-    const res = NextResponse.next();
-    console.log(`Request count: ${requestCount}`);
-    return res;
+// // This middleware runs on all routes
+// export function middleware(req: NextRequest) {
+//     requestCount++;
+//     const res = NextResponse.next();
+//     console.log(`Request count: ${requestCount}`);
+//     return res;
+// }
+
+// // To selectively run middleware only on certain paths:
+
+// export const config = {
+//     matcher: '/api/:path*'    // Only runs on /api/anything
+// }
+
+
+// If we want to run logic more selectively by filtering the specified paths
+
+export function middleware(request: NextRequest) {
+    console.log(request.nextUrl.pathname)
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+        return NextResponse.redirect(new URL('/signin', request.url))
+    }
+
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+        return NextResponse.next()
+    }
 }
-
-// To selectively run middleware only on certain paths:
-
-export const config = {
-    matcher: '/api/:path*'    // Only runs on /api/anything
-}
-
-
