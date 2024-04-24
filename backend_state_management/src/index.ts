58d6b57851386fb7a5b1160cbb37c4103453b3cd
Prathyarthi <prathyarti@gmail.com>
2024-04-24 18:05:10 +0530

    State management in backend

---
+++ backend_state_management/src/index.ts
@@ -0,0 +1,14 @@
+import { games } from "./store";
+import { startLogger } from "./logger";
+
+startLogger()
+
+
+setInterval(() => {
+    games.push({
+        id: Math.random().toString(),
+        whitePlayerName: 'prathyarthi',
+        blackPlayerName: 'kiran',
+        moves: []
+    })
+}, 5000)
